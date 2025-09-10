47eee126584780bfc4f62eb9798185ade86d4cb0
adotri-frimpong <goldmalcom43@gmail.com>
2025-09-10 17:14:55 +0200

    [FEAT]: Update of utilities package | GenerateRandomNumber() function and game session logic

--- number-guessing-game/utilities/utilities.go
+++ number-guessing-game/utilities/utilities.go
@@ -1,28 +1,31 @@
 package utilities
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand/v2"
+)
 
 
-type DiffInfos struct {
-	Difficulty string
-	NumberOfAttempts uint
+type diffInfos struct {
+	difficulty string
+	numberOfAttempts int
 }
 
 
-func GetDifficulties() map[uint]DiffInfos {
-	choices := make(map[uint]DiffInfos)
+func GetDifficulties() map[uint]diffInfos {
+	choices := make(map[uint]diffInfos)
 
-	easy := DiffInfos {
-		Difficulty: "Easy",
-		NumberOfAttempts: 10,
+	easy := diffInfos {
+		difficulty: "Easy",
+		numberOfAttempts: 10,
 	}
-	medium := DiffInfos {
-		Difficulty: "Medium",
-		NumberOfAttempts: 5,
+	medium := diffInfos {
+		difficulty: "Medium",
+		numberOfAttempts: 5,
 	}
-	hard := DiffInfos {
-		Difficulty: "Hard",
-		NumberOfAttempts: 3,
+	hard := diffInfos {
+		difficulty: "Hard",
+		numberOfAttempts: 3,
 	}
 
 	choices[1] = easy
@@ -37,10 +40,11 @@ func GreetUser() {
 	fmt.Println("Welcome to the Number Guessing Game!")
 	fmt.Println("I am thinking of a number between 1 and 100.")
 	fmt.Println("You have 5 chances to guess the correct number.")
+	fmt.Print("\n")
 }
 
 
-func SelectDifficulty(difficulties map[uint]DiffInfos) uint {
+func SelectDifficulty(difficulties map[uint]diffInfos) int {
 	var choice uint
 
 	for {
@@ -48,15 +52,42 @@ func SelectDifficulty(difficulties map[uint]DiffInfos) uint {
 		fmt.Println("1. Easy (10 chances)")
 		fmt.Println("2. Medium (5 chances)")
 		fmt.Println("3. Hard (3 chances)")
-		fmt.Printf("Enter your choice: ")
+		fmt.Print("\nEnter your choice: ")
 		fmt.Scan(&choice)
 		if (choice < 1) || (choice > 3) {
 			fmt.Println("Choose a correct number")
+			fmt.Print("\n")
 			continue
 		} else {
-			fmt.Printf("Great! You have selected the %v difficulty level.\n", difficulties[choice].Difficulty)
+			fmt.Printf("\nGreat! You have selected the %v difficulty level.\n", difficulties[choice].difficulty)
+			fmt.Println("Let's start the game!")
 			break
 		}
 	}
-	return choice
+	return difficulties[choice].numberOfAttempts
+}
+
+func GenerateRandomNumber() uint {
+	return uint(rand.IntN(100)+1)
+}
+
+func GameSession(numberToGuess uint, userNumber uint, diff int) {
+	for i := 0; i < diff; i++ {
+		fmt.Println(numberToGuess)
+		fmt.Print("\nEnter your guess: ")
+		fmt.Scan(&userNumber)
+		if userNumber == numberToGuess {
+			fmt.Printf("Congratulations! You guessed the correct number in %v attempts.\n", i+1)
+			break
+		} else if userNumber > numberToGuess {
+			fmt.Printf("Incorrect! The number is less than %v.\n", userNumber)
+		} else if userNumber < numberToGuess {
+			fmt.Printf("Incorrect! The number is greater than %v.\n", userNumber)
+		}
+	}
+	if numberToGuess != userNumber {
+		fmt.Print("\n##############################\n")
+		fmt.Printf("Sorry, you've used all your attempts. The correct number was %v.\n", numberToGuess)
+		fmt.Print("##############################\n")
+	}
 }
