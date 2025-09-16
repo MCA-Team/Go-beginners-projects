e07546683ab7814f22d454a46b566645943963f8
adotri-frimpong <goldmalcom43@gmail.com>
2025-09-16 23:26:00 +0200

    [FEAT]: Update of main.go file | Connexion to the database

--- blogging-platform-api/main.go
+++ blogging-platform-api/main.go
@@ -4,21 +4,31 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"database/sql"
 )
 
-// type blog struct {
-// 	title string
-// 	content string
-// 	category string
-// 	tags []string
-// }
+type blog struct {
+	title string		`json:"title"`
+	content string		`json:"content"`
+	category string		`json:"category"`
+	tags []string		`json:"tags"`
+}
 
 func main() {
 	fmt.Println("BLOGGING PLATFORM API")
+
+	// Connecting to the database
+	db, err := sql.Open("postgres", "user=postgres password=yourpassword dbname=blogging_platform sslmode=disable")
+	if err != nil {
+		log.Fatal("Failed to connect to the database", err)
+	}
+	defer db.Close()
 	
 	// Starting a HTTP server
 	err := http.ListenAndServe(":8008", nil)
 	if err != nil {
 		log.Fatal("Failed to start the HTTP server", err)
 	}
+
+	
 }
