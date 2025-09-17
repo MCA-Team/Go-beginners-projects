277c2fd649168f954a4e21c536ed05049ceca9d9
adotri-frimpong <goldmalcom43@gmail.com>
2025-09-17 18:07:16 +0200

    [FEAT]: Update of main.go file | HTTP server + middleware + router with Gin package

--- blogging-platform-api/main.go
+++ blogging-platform-api/main.go
@@ -2,33 +2,21 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
-	"database/sql"
+	"github.com/gin-gonic/gin"
 )
 
-type blog struct {
-	title string		`json:"title"`
-	content string		`json:"content"`
-	category string		`json:"category"`
-	tags []string		`json:"tags"`
-}
+
 
 func main() {
 	fmt.Println("BLOGGING PLATFORM API")
 
-	// Connecting to the database
-	db, err := sql.Open("postgres", "user=postgres password=yourpassword dbname=blogging_platform sslmode=disable")
-	if err != nil {
-		log.Fatal("Failed to connect to the database", err)
-	}
-	defer db.Close()
-	
-	// Starting a HTTP server
-	err := http.ListenAndServe(":8008", nil)
-	if err != nil {
-		log.Fatal("Failed to start the HTTP server", err)
-	}
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+		"message": "pong",
+		})
+	})
+	router.Run() // listen and serve on 0.0.0.0:8080
 
 	
 }
