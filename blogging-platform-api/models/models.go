7c850b01c1f01f23b0fd03b3c65026bc949e8a3d
adotri-frimpong <goldmalcom43@gmail.com>
2025-09-17 18:08:09 +0200

    [FEAT]: Creation of models package | Package for database ORM purposes

---
+++ blogging-platform-api/models/models.go
@@ -0,0 +1,15 @@
+package models
+
+// import (
+// 	"gorm.io/gorm"
+// )
+
+
+// type Blog struct {
+// 	gorm.Model
+// 	title string		`json:"title"`
+// 	content string		`json:"content"`
+// 	category string		`json:"category"`
+// 	tags []string		`json:"tags"`
+// }
+
