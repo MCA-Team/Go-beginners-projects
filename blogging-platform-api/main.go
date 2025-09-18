bca7456239b1296e10a39eff0b4d11b224687b56
adotri-frimpong <goldmalcom43@gmail.com>
2025-09-18 22:28:43 +0200

    [FEAT]: Update of main.go file | Connection to SQLite database and Automigration

--- blogging-platform-api/main.go
+++ blogging-platform-api/main.go
@@ -2,21 +2,39 @@ package main
 
 import (
 	"fmt"
-	"github.com/gin-gonic/gin"
+	"github.com/glebarez/sqlite"
+	"gorm.io/gorm"
+	"blogging-platform-api/models"
+	// "net/http"
+	// "github.com/gin-gonic/gin"
 )
 
 
+ 
 
 func main() {
 	fmt.Println("BLOGGING PLATFORM API")
 
-	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-		"message": "pong",
-		})
-	})
-	router.Run() // listen and serve on 0.0.0.0:8080
+	db, err := gorm.Open(sqlite.Open("blog.db"), &gorm.Config{})
+	if err != nil {
+		panic("Failed to connect to the dabase")
+	}
+	db.AutoMigrate(&models.Post{})
+	
+
+	// router := gin.Default()
+	// router.GET("/", func(c *gin.Context) {
+	// 	c.JSON(http.StatusOK, gin.H{
+	// 	"message": "pong",
+	// 	})
+	// })
+
+	// router.GET("/ap", func(c *gin.Context) {
+	// 	c.JSON(http.StatusOK, gin.H{
+	// 	"apellidos": "Perez Casado",
+	// 	})
+	// })
+	// router.Run(":3000") // listen and serve on 0.0.0.0:3000
 
 	
 }
