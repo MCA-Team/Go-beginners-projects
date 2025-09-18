0a371a6e04cf06531df4f662fc30442069300acc
adotri-frimpong <goldmalcom43@gmail.com>
2025-09-18 22:29:54 +0200

    [FEAT]: Creation of models package | Post model setting up and definition of posts creation function

--- blogging-platform-api/models/models.go
+++ blogging-platform-api/models/models.go
@@ -1,15 +1,26 @@
 package models
 
-// import (
-// 	"gorm.io/gorm"
-// )
+import "gorm.io/gorm"
+
+type Post struct {
+	gorm.Model
+	Title string
+	Content string
+	Category string
+	Tags []string
+}
 
 
-// type Blog struct {
-// 	gorm.Model
-// 	title string		`json:"title"`
-// 	content string		`json:"content"`
-// 	category string		`json:"category"`
-// 	tags []string		`json:"tags"`
-// }
-
+// CreatePost creates a new post with passed title, body, category and tags elements in argument and commits it in the db in argument too through GORM.
+func CreatePost(db *gorm.DB, title, body, category string, tags []string) Post {
+	newPost := Post {
+		Title: title,
+		Content: body,
+		Category: category,
+		Tags: tags,
+	}
+	if result := db.Create(&newPost); result.Error != nil {
+		panic("Failed to create a new element in the database !")
+	}
+	return newPost
+}
