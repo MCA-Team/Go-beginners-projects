08144b11cc4ee437a60368a255f56792fc1836ed
adotri-frimpong <goldmalcom43@gmail.com>
2025-09-20 22:36:17 +0200

    [FEAT]: Update of main.go file | GET method and route testing for getting all retrieved posts

--- blogging-platform-api/main.go
+++ blogging-platform-api/main.go
@@ -3,9 +3,7 @@ package main
 import (
 	"blogging-platform-api/models"
 	"fmt"
-	"log"
 	"net/http"
-
 	"github.com/gin-gonic/gin"
 	"github.com/glebarez/sqlite"
 	"gorm.io/gorm"
@@ -24,27 +22,34 @@ func main() {
 
 	db.AutoMigrate(&models.Post{})
 
-	// post := models.CreatePost(db, "title", "the body", "the category", "['tag1', 'tag2', 'tag3']")
+	// post := models.CreatePost(db, "title", "the body", "the category", []string{"tag1", "tag2", "tag3"})
 	// fmt.Println(post)
 	
-
+	query := db.Find(&models.Post{})
+	fmt.Println(query.RowsAffected)
 	router := gin.Default()
-	router.POST("/posts", func(c *gin.Context) {
-		var inputData struct {
-			title string 
-			content string 
-			category string
-			tags string	
-		}
-
+	router.POST("/posts", func (c *gin.Context) {
+		
+		var inputData models.Post
 		c.Bind(&inputData)
+		post := models.CreatePost(db, inputData.Title, inputData.Content, inputData.Category, inputData.Tags)
 
-		post := models.CreatePost(db, inputData.title, inputData.content, inputData.category, inputData.tags)
-		log.Println(inputData)
 		c.JSON(http.StatusOK, gin.H{
 		"post": post,
 		})
 	})
+
+
+	router.GET("/posts", func (c *gin.Context) {
+		
+		var posts []models.Post
+		db.Find(&posts)
+
+		c.JSON(http.StatusOK, gin.H{
+		"post": posts,
+		})
+	})
+
 
 	router.Run(":3000") // listen and serve on 0.0.0.0:3000
 
