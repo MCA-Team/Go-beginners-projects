76899e3d4a4fa068a05bef2455a3930137eca53f
adotri-frimpong <goldmalcom43@gmail.com>
2025-09-20 22:36:42 +0200

    [REFACTOR]: Update of models package | Code refactoring

--- blogging-platform-api/models/models.go
+++ blogging-platform-api/models/models.go
@@ -7,12 +7,12 @@ type Post struct {
 	Title string
 	Content string
 	Category string
-	Tags string
+	Tags []string `gorm:"serializer:json"`	// Serialization allows the dabase to store slice of stings correctly
 }
 
 
 // CreatePost creates a new post with passed title, body, category and tags elements in argument and commits it in the db in argument too through GORM.
-func CreatePost(db *gorm.DB, title, body, category string, tags string) Post {
+func CreatePost(db *gorm.DB, title, body, category string, tags []string) Post {
 	newPost := Post {
 		Title: title,
 		Content: body,
