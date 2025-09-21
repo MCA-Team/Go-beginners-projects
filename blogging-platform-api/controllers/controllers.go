9ca6aa40f4537d53875d629b8eace1080b3ccaec
adotri-frimpong <goldmalcom43@gmail.com>
2025-09-21 22:24:40 +0200

    [FEAT]: Update of controllers package | Addition of POST one article and GET all articles functions

--- blogging-platform-api/controllers/controllers.go
+++ blogging-platform-api/controllers/controllers.go
@@ -1,1 +1,27 @@
 package controllers
+
+import (
+	"blogging-platform-api/models"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func PostOneArticle(c *gin.Context) {
+		var inputData models.Article
+		c.Bind(&inputData)
+		article := models.CreatePost(models.DB, inputData.Title, inputData.Content, inputData.Category, inputData.Tags)
+
+		c.JSON(http.StatusOK, gin.H{
+		"Article": article,
+		})
+	}
+
+func GetAllArticles(c *gin.Context) {
+		var articles []models.Article
+		models.DB.Find(&articles)
+
+		c.JSON(http.StatusOK, gin.H{
+		"Articles": articles,
+		})
+	}
+
