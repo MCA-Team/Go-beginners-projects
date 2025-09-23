ecdc9431f0afd965632801d624234b199a192f7c
adotri-frimpong <goldmalcom43@gmail.com>
2025-09-23 15:38:40 +0200

    [REFACTOR]: Update of models package | Model binding

--- blogging-platform-api/models/models.go
+++ blogging-platform-api/models/models.go
@@ -9,10 +9,10 @@ import (
 
 type Article struct {
 	gorm.Model
-	Title string
-	Content string
-	Category string
-	Tags []string `gorm:"serializer:json"`	// Serialization allows the dabase to store slice of stings correctly
+	Title string	`form:"title" json:"title" xml:"title" binding:"required"`
+	Content string	`form:"content" json:"content" xml:"content" binding:"required"`
+	Category string	`form:"category" json:"category" xml:"category"`
+	Tags []string `form:"tags" json:"tags" xml:"tags" gorm:"serializer:json"`	// Serialization allows the dabase to store slice of stings correctly
 }
 
 var DB *gorm.DB
@@ -26,18 +26,4 @@ func ConnectToDB() {
 		fmt.Println("DB CONNECTION ESTABLISHED")
 	}
 	
-}
-
-// CreatePost creates a new post with passed title, body, category and tags elements in argument and commits it in the DB in argument too through GORM.
-func CreatePost(DB *gorm.DB, title, body, category string, tags []string) Article {
-	newArticle := Article {
-		Title: title,
-		Content: body,
-		Category: category,
-		Tags: tags,
-	}
-	if result := DB.Create(&newArticle); result.Error != nil {
-		panic(result.Error)
-	}
-	return newArticle
 }
